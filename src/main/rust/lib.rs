4ed7704ff0744532901f18fcdc55823a360b57cc
zaaarf <zaaarf@proton.me>
2023-09-07 00:52:05 +0200

    fix: include glue module
    Co-authored-by: alemi <me@alemi.dev>

--- src/main/rust/lib.rs
+++ src/main/rust/lib.rs
@@ -5,58 +5,62 @@ use codemp::prelude::*;
 use rifgen::rifgen_attr::generate_interface;
 use crate::error::ErrorWrapper;
 
+pub mod glue {
+	include!(concat!(env!("OUT_DIR"), "/glue.rs"));
+}
+
 // #[generate_interface_doc] //TODO
 struct CodeMPHandler {}
 
 impl CodeMPHandler {
 	#[generate_interface(constructor)]
-	async fn new() -> CodeMPHandler {
+	fn new() -> CodeMPHandler {
 		CodeMPHandler {}
 	}
 
 	#[generate_interface]
-	async fn connect(addr: String) -> Result<(), String> {
-		convert(CODEMP_INSTANCE.connect(&addr).await)
+	fn connect(addr: String) -> Result<(), String> {
+		convert(CODEMP_INSTANCE.connect(&addr))
 	}
 
 	#[generate_interface]
-	async fn join(session: String) -> Result<CursorHandler, String> {
-		convert_cursor(CODEMP_INSTANCE.join(&session).await)
+	fn join(session: String) -> Result<CursorHandler, String> {
+		convert_cursor(CODEMP_INSTANCE.join(&session))
 	}
 
 	#[generate_interface]
-	async fn create(path: String) -> Result<(), String> {
-		convert(CODEMP_INSTANCE.create(&path, None).await)
+	fn create(path: String) -> Result<(), String> {
+		convert(CODEMP_INSTANCE.create(&path, None))
 	}
 
 	#[generate_interface]
-	async fn create_with_content(path: String, content: String) -> Result<(), String> {
-		convert(CODEMP_INSTANCE.create(&path, Some(&content)).await)
+	fn create_with_content(path: String, content: String) -> Result<(), String> {
+		convert(CODEMP_INSTANCE.create(&path, Some(&content)))
 	}
 
 	#[generate_interface]
-	async fn attach(path: String) -> Result<BufferHandler, String> {
-		convert_buffer(CODEMP_INSTANCE.attach(&path).await)
+	fn attach(path: String) -> Result<BufferHandler, String> {
+		convert_buffer(CODEMP_INSTANCE.attach(&path))
 	}
 
 	#[generate_interface]
-	async fn get_cursor() -> Result<CursorHandler, String> {
-		convert_cursor(CODEMP_INSTANCE.get_cursor().await)
+	fn get_cursor() -> Result<CursorHandler, String> {
+		convert_cursor(CODEMP_INSTANCE.get_cursor())
 	}
 
 	#[generate_interface]
-	async fn get_buffer(path: String) -> Result<BufferHandler, String> {
-		convert_buffer(CODEMP_INSTANCE.get_buffer(&path).await)
+	fn get_buffer(path: String) -> Result<BufferHandler, String> {
+		convert_buffer(CODEMP_INSTANCE.get_buffer(&path))
 	}
 
 	#[generate_interface]
-	async fn leave_workspace() -> Result<(), String> {
-		convert(CODEMP_INSTANCE.leave_workspace().await)
+	fn leave_workspace() -> Result<(), String> {
+		convert(CODEMP_INSTANCE.leave_workspace())
 	}
 
 	#[generate_interface]
-	async fn disconnect_buffer(path: String) -> Result<bool, String> {
-		convert(CODEMP_INSTANCE.disconnect_buffer(&path).await)
+	fn disconnect_buffer(path: String) -> Result<bool, String> {
+		convert(CODEMP_INSTANCE.disconnect_buffer(&path))
 	}
 }
 
@@ -73,24 +77,25 @@ fn convert<T>(result: Result<T, CodempError>) -> Result<T, String> {
 }
 
 struct CursorHandler {
+	#[allow(unused)]
 	cursor: Option<Arc<CodempCursorController>>
 }
 
 impl CursorHandler {
 	#[generate_interface(constructor)]
-	async fn new() -> CursorHandler { //TODO: this sucks but whatever
+	fn new() -> CursorHandler { //TODO: this sucks but whatever
 		panic!("Default constructor for CursrorHandler should never be called!")
 	}
 }
 
-
 struct BufferHandler {
+	#[allow(unused)]
 	buffer: Option<Arc<CodempBufferController>>
 }
 
 impl BufferHandler {
 	#[generate_interface(constructor)]
-	async fn new() -> BufferHandler { //TODO: this sucks but whatever
+	fn new() -> BufferHandler { //TODO: this sucks but whatever
 		panic!("Default constructor for BufferHandler should never be called!")
 	}
 }
